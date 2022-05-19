6f953da7ff8b6bacbfb0d4604235be42d9e9b644
aluno <Aluno@DEVE003018.educacional.up>
2022-05-19 11:50:17 -0300

    pronto

--- ExSQLite/src/br/edu/up/Programa.java
+++ ExSQLite/src/br/edu/up/Programa.java
@@ -5,10 +5,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Programa {
 
 	public static void main(String[] args) {
+		
 		
 		
 		String url = "jdbc:sqlite:C:\\bancodedados\\primeiro_bd_sqlite.db";
@@ -16,7 +18,34 @@ public class Programa {
 			
 			Connection con = DriverManager.getConnection(url);//conexão criada.
 			Statement stmt=con.createStatement();
+			
+			Scanner leitor=new Scanner(System.in);
+			
+			System.out.println("Informe o nome da pessoa: ");
+			String nomeEntrada=leitor.nextLine();
+			
+			System.out.println("Informe o cpf da pessoa: ");
+			String cpfEntrada=leitor.nextLine();
+			
+			System.out.println("Informe a data de nascimento da pessoa: ");
+			String dataEntrada=leitor.nextLine();
+			
 			String querySelect="select * from pessoas ";//criei a variavel 
+			
+			String queryInsert="insert into pessoas(nome,cpf,dtanasc) values('"+nomeEntrada+"','"+cpfEntrada+"','"+dataEntrada+"')";
+			Statement stmtInsert=con.createStatement();
+			stmtInsert.execute(queryInsert);
+			
+			/*String queryUpdate="update pessoas set nome='carlos silva' where id='3'";
+			Statement stmtUpdate=con.createStatement();
+			stmt.execute(queryUpdate);
+			
+			String queryDelete="delete from pessoas where id=2";
+			Statement stmtDelete=con.createStatement();
+			stmt.execute(queryDelete);*/
+			
+			
+			
 			ResultSet resultado=stmt.executeQuery(querySelect);//vai trazer o resultado
 			
 			while(resultado.next()) {
@@ -36,7 +65,7 @@ public class Programa {
 		} catch (SQLException e) {//caso de erro 
 		System.out.println("------->ERRO:Falha na conexão com o banco de dados");//surround tratando o erro
 		System.out.println(e.getMessage());//mostra mais simplificado
-		/*e.printStackTrace();//imprima para mim toda pilha de erro */
+		e.printStackTrace();//imprima para mim toda pilha de erro */
 		}//se não conseguir a conexão vai dar falha
 		//throws-deixar para lá,surround fazer o tratamento do erro
 		
